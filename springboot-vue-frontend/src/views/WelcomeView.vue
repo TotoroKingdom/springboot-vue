e3aa203d4e1cc9b2189ef5f2a45430d6e4072fd2
TotoroKingdom <634581304@qq.com>
2023-12-10 03:16:12 +0800

    基本页面配置

---
+++ springboot-vue-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+
+<template>
+
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
+
+      <div style="flex: 1; background-color: black">
+        <el-image style="width: 100%; height: 100%" fit="cover"
+                  src="./src/assets/img.png">
+
+        </el-image>
+
+      </div>
+
+      <div class="welcome-title">
+          <div style="font-size: 20px; font-weight: bold">
+              尽量不要，倘若没有必要理由，更是绝对不能，对任何人说出或写下“我太忙”这几个字；也不要用类似借口，推说“周围压力太大”，时常逃避我们对同辈和同龄人应尽的义务。</div>
+
+      </div>
+
+
+
+      <div class="right-card">
+
+
+      </div>
+
+  </div>
+
+</template>
+
+
+<style scoped>
+
+.welcome-title{
+    position: absolute;
+    bottom: 30px;
+    left: 30px;
+    color: orangered;
+    text-shadow: 0 0 10px black;
+}
+
+.right-card{
+    width: 400px;
+    z-index: 1;
+    background-color: white;
+}
+
+</style>
---
+++ springboot-vue-frontend/src/views/welcome/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div>
+      我是登录页面
+  </div>
+
+</template>
+
+<script>
+export default {
+    name: "LoginPage"
+}
+</script>
+
+<style scoped>
+
+</style>
