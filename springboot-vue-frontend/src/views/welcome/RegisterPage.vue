db21c918a9dc29018f9ce4d0fd41dccd616334f9
TotoroKingdom <634581304@qq.com>
2023-12-14 00:02:05 +0800

    前端：完善注册操作

--- springboot-vue-frontend/src/views/welcome/RegisterPage.vue
+++ springboot-vue-frontend/src/views/welcome/RegisterPage.vue
@@ -1,7 +1,13 @@
 <script setup>
-import {reactive} from "vue";
+import {computed, reactive, ref} from "vue";
 import {EditPen, Lock, Message, User} from "@element-plus/icons-vue";
 import router from "@/router";
+import {ElMessage} from "element-plus";
+import {get, post} from "@/net";
+
+const coldTime = ref()
+
+const formRef = ref()
 
 const form = reactive({
     username:'',
@@ -12,7 +18,79 @@ const form = reactive({
 
 })
 
+const validateUsername = (rule, value, callback) => {
+    if (value === ''){
+        callback(new Error('请输入用户名'))
+    } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)){
+        callback(new Error('用户名不能包含特殊字符，只能是中英文'))
+    } else {
+        callback()
+    }
+}
+
+const validatePassword = (rule, value, callback) =>{
+    if (value === ''){
+        callback(new Error("请再次输入密码"))
+    }else if (value !== form.password){
+        callback(new Error("两次输入的密码不一致"))
+    }else {
+        callback()
+    }
+}
+
 const rule = {
+    username:[
+        { validator: validateUsername, trigger: ['blur','change']},
+        // {min: 1 , max: 10, message: '用户名的长度必须在2~8个字符之间', trigger: ['blur','change']}
+    ],
+    password:[
+        { required: true, message:'请输入密码', trigger: ['blur','change']},
+        {min: 6 , max: 20, message: '密码的长度必须在6~20个字符之间', trigger: ['blur','change']}
+    ],
+    password_repeat:[
+        { validator: validatePassword, trigger: ['blur','change']}
+    ],
+    email:[
+        { required: true,message:'请输入电子邮箱', trigger: ['blur']},
+        { type:'email', message: '请输入合法的电子邮件地址', trigger: ['blur','change']}
+    ],
+    code:[
+        { required: true,message:'请输入获取的验证码', trigger: ['blur']},
+    ]
+
+}
+
+function askCode(){
+    if (/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/.test(form.email)){
+        coldTime.value = 60
+        get(`/api/auth/ask-code?email=${form.email}&type=register`, () => {
+            ElMessage.success(`验证码已发送到邮箱：${form.email},请注意查收`)
+            setInterval(() => coldTime.value--, 1000)
+        }, (message) => {
+            ElMessage.warning(message)
+            coldTime.value = 0
+        })
+    } else {
+        ElMessage.warning("请输入正确的电子邮件！")
+    }
+
+}
+
+const isEmailValid = computed(() => {
+    return /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/.test(form.email)
+})
+
+function register(){
+    formRef.value.validate((valid) =>{
+        if (valid){
+            post('/api/auth/register',{...form}, ()=>{
+                ElMessage.success('注册成功，欢迎加入我们')
+                router.push('/')
+            })
+        }else {
+            ElMessage.warning('请完整填写注册表单内容')
+        }
+    })
 
 }
 
@@ -27,8 +105,8 @@ const rule = {
     </div>
 
     <div style="margin-top: 50px">
-        <el-form :model="form" :rules="rule">
-            <el-form-item>
+        <el-form :model="form" :rules="rule" ref="formRef">
+            <el-form-item prop="username">
                 <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名">
                     <template #prefix>
                         <el-icon><User/></el-icon>
@@ -36,7 +114,7 @@ const rule = {
                 </el-input>
             </el-form-item>
 
-            <el-form-item>
+            <el-form-item prop="password">
                 <el-input v-model="form.password" maxlength="20" type="password" placeholder="密码">
                     <template #prefix>
                         <el-icon><Lock/></el-icon>
@@ -44,7 +122,7 @@ const rule = {
                 </el-input>
             </el-form-item>
 
-            <el-form-item>
+            <el-form-item prop="password_repeat">
                 <el-input v-model="form.password_repeat" maxlength="20" type="password" placeholder="重复密码">
                     <template #prefix>
                         <el-icon><Lock/></el-icon>
@@ -52,15 +130,15 @@ const rule = {
                 </el-input>
             </el-form-item>
 
-            <el-form-item>
-                <el-input v-model="form.email" maxlength="20" type="email" placeholder="电子邮箱">
+            <el-form-item prop="email">
+                <el-input v-model="form.email" type="email" placeholder="电子邮箱">
                     <template #prefix>
                         <el-icon><Message/></el-icon>
                     </template>
                 </el-input>
             </el-form-item>
 
-            <el-form-item>
+            <el-form-item prop="code">
                 <el-row :gutter="10" style="width: 100%">
                     <el-col :span="17">
                         <el-input v-model="form.code" maxlength="6" type="text" placeholder="请输入验证码">
@@ -70,7 +148,9 @@ const rule = {
                         </el-input>
                     </el-col>
                     <el-col :span="5">
-                        <el-button type="success">获取验证码</el-button>
+                        <el-button @click="askCode" :disabled="!isEmailValid || coldTime" type="success">
+                            {{ coldTime > 0 ? `请稍后${coldTime}秒`:'获取验证码'}}
+                        </el-button>
                     </el-col>
                 </el-row>
             </el-form-item>
@@ -78,7 +158,7 @@ const rule = {
     </div>
 
       <div style="margin-top: 80px">
-          <el-button style="width: 270px" type="warning">立即注册</el-button>
+          <el-button @click="register" style="width: 270px" type="warning">立即注册</el-button>
       </div>
       <div style="margin-top: 20px">
           <span style="font-size: 14px; line-height: 15px; color: grey">已有账号？</span>
